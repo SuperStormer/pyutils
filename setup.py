0158253ae4130a249ab4d3c7c46b6e6ab37e06e0
Larry Xue <larry.p.xue@gmail.com>
2018-10-19 13:25:28 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+	name="pyutils",
+	version="0.1",
+	descripton="my util functions for python",
+	license="GPLv3",
+	author="SuperStormer",
+	author_email="larry.p.xue@gmail.com",
+	url="https://github.com/SuperStormer/pyutils",
+	project_urls={
+		"Source Code":"https://github.com/SuperStormer/pyutils"
+	}
+)
