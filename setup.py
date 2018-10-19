5e087ff07bb6d71a21acc8bac6bc85b3f1cbdafb
Larry Xue <larry.p.xue@gmail.com>
2018-10-19 13:58:31 -0400

    fixed setuptools not finding py files

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(
 	name="pyutils",
 	version="0.1",
 	descripton="my util functions for python",
+	packages=find_packages(),
 	license="GPLv3",
 	author="SuperStormer",
 	author_email="larry.p.xue@gmail.com",
