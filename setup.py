57a27084a53d140612abcc442ae4344b4887bb9e
Larry Xue <larry.p.xue@gmail.com>
2018-11-09 12:52:39 -0500

    housekeeping

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 setup(
-	name="pyutils",
-	version="0.1",
+	name="miscutil",
+	version="0.6",
 	descripton="my util functions for python",
 	packages=find_packages(),
 	license="GPLv3",
 	author="SuperStormer",
 	author_email="larry.p.xue@gmail.com",
-	url="https://github.com/SuperStormer/pyutils",
-	project_urls={
-		"Source Code":"https://github.com/SuperStormer/pyutils"
-	}
+	url="https://github.com/SuperStormer/miscutil",
+	project_urls={"Source Code": "https://github.com/SuperStormer/miscutil"}
 )
