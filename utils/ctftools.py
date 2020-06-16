0a5dad8b3541ebf68e8955b90e1c671aff939670
Larry Xue <larry.p.xue@gmail.com>
2020-06-15 21:16:01 -0400

    improved pyjail

--- utils/ctftools.py
+++ utils/ctftools.py
@@ -48,21 +48,28 @@ class PyJail:
 		subclasses = "''.__class__.mro()[1].__subclasses__()"
 		self.builtins = f"{subclasses}[-1].__init__.__globals__['__builtins__']"
 	
-	def exec(self, s: str):
+	def run(self, s: str):
 		"""directly run with s appended to self.builtins"""
 		return self.fn(self.builtins + s)
 	
 	def call(self, fn: str, *args, module=None):
-		if module is None:  # call builtin
-			return self.exec(f"[{fn!r}](*{args!r})")
-		else:  # import module and then call function
-			return self.exec(f"['__import__']({module!r}).{fn}(*{args!r})")
+		"""call function"""
+		return self.run(self._call(fn, *args, module))
 	
-	def cat(self, fn):
-		return self.exec(f"['open']({fn!r}).read()")
+	def cat(self, filename):
+		return self.run(self._call("open", filename) + ".read()")
 	
 	def ls(self, folder="."):
 		return self.call("listdir", folder, module="os")
 	
 	def shell(self, cmd):
-		return self.exec(f"['__import__']('os').popen({cmd!r}).read()")
+		return self.run(self._call("popen", cmd, module="os") + ".read()")
+	
+	def _import(self, module):
+		return f"['__import__']({module!r})"
+	
+	def _call(self, fn: str, *args, module=None):
+		if module is None:  # call builtin
+			return f"[{fn!r}](*{args!r})"
+		else:  # import module and then call function
+			return self._import(module) + f".{fn}(*{args!r})"
