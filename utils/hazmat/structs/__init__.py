fde0c6e7ba371b90014647420c3ce44dff92a512
Larry Xue <larry.p.xue@gmail.com>
2020-11-25 22:44:00 -0500

    fixed wildcard imports

--- utils/hazmat/structs/__init__.py
+++ utils/hazmat/structs/__init__.py
@@ -1,9 +1,1 @@
-from .common import *  # this must be first to avoid circular imports
-
-from .base import *
-from .bytes import *
-from .collections import *
-from .dict import *
-from .iterators import *
-from .num import *
-from .str import *
+__all__ = []  # avoid wildcard importing and overwriting builtins
---
+++ utils/hazmat/structs/all.py
@@ -0,0 +1,10 @@
+from .common import *  # this must be first to avoid circular imports
+
+from .base import *
+from .bytes import *
+
+from .collections import *
+from .dict import *
+from .iterators import *
+from .num import *
+from .str import *
