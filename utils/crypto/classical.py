5b691fa6b114b35d8ea9488410210c11b29179a8
Larry Xue <larry.p.xue@gmail.com>
2020-12-29 15:33:15 -0500

    make caesar and xor decrypt to return namedtuples

--- utils/crypto/classical.py
+++ utils/crypto/classical.py
@@ -1,12 +1,17 @@
 import string
 import itertools
 from .misc import english_like
+from typing import NamedTuple
+
+class CaesarResult(NamedTuple):
+	text: str
+	key: int
 
 def caesar_all(s, alphabet_only=True):
 	if alphabet_only:
-		return ((caesar(s, i, alphabet_only), i) for i in range(26))
+		return (CaesarResult(caesar(s, i, alphabet_only), i) for i in range(26))
 	else:  #full ascii
-		return ((caesar(s, i, alphabet_only), i) for i in range(128))
+		return (CaesarResult(caesar(s, i, alphabet_only), i) for i in range(128))
 
 def caesar(s, shift, alphabet_only=True):
 	if alphabet_only:
@@ -19,7 +24,6 @@ def caesar(s, shift, alphabet_only=True):
 		return "".join(chr((ord(c) + shift) % 128) for c in s if (ord(c) + shift) % 128 > 31)
 
 def decrypt_caesar(s, alphabet_only=True):
-	"""returns (plaintext,key) """
 	return max(caesar_all(s, alphabet_only), key=lambda x: english_like(x[0]))
 
 def vigenere(s, key, mode):
@@ -30,9 +34,11 @@ def vigenere(s, key, mode):
 			65) if c.isupper() else chr((ord(c) + string.ascii_lowercase.index(d) - 97) % 26 +
 			97) if c.islower() else c for c, d in zip(s, itertools.cycle(key))
 		)
-	else:
+	elif mode == "decrypt":
 		return "".join(
 			chr((ord(c) - string.ascii_lowercase.index(d) - 65) % 26 +
 			65) if c.isupper() else chr((ord(c) - string.ascii_lowercase.index(d) - 97) % 26 +
 			97) if c.islower() else c for c, d in zip(s, itertools.cycle(key))
 		)
+	else:
+		raise ValueError(f"Illegal mode {mode!r} is not 'decrypt' or 'encrypt'")
--- utils/crypto/xor.py
+++ utils/crypto/xor.py
@@ -1,23 +1,29 @@
 import itertools
-from .misc import english_like, hamming
+from typing import NamedTuple
+
 from utils.itertools2 import grouper
 
-def xor(s, t):
+from .misc import english_like, hamming
+
+class XorResult(NamedTuple):
+	text: bytes
+	key: bytes
+
+def xor(s, t) -> bytes:
 	return bytes(a ^ b for a, b in zip(s, t))
 
-def one_byte_xor_all(s):
-	"""returns (plaintext,key)"""
-	return ((xor(s, itertools.repeat(c)).decode("utf-8", errors="ignore"), c) for c in range(256))
+def one_byte_xor_all(s: bytes):
+	return (XorResult(xor(s, itertools.repeat(c)), bytes(c)) for c in range(256))
 
-def decrypt_one_byte_xor(s):
-	"""returns (plaintext,key)"""
-	return max(one_byte_xor_all(s), key=lambda x: english_like(x[0]))
+def decrypt_one_byte_xor(s: bytes) -> XorResult:
+	return max(
+		one_byte_xor_all(s), key=lambda x: english_like(x.text.decode("utf-8", errors="ignore"))
+	)
 
 def repeating_key_xor(s, k):
 	return xor(s, itertools.cycle(k))
 
-def repeating_key_xor_all(s, min_len=2, max_len=40, keysizes_to_try=3):
-	"""returns (plaintext,key)"""
+def repeating_key_xor_all(s, min_len=2, max_len=40, keysizes_to_try=3) -> list[XorResult]:
 	#average hamming distance of first 4 key_size len chunks
 	key_sizes = sorted(
 		range(min_len, max_len + 1),
@@ -29,12 +35,11 @@ def repeating_key_xor_all(s, min_len=2, max_len=40, keysizes_to_try=3):
 	for key_size in key_sizes[:keysizes_to_try]:
 		blocks = list(grouper(s, key_size, fillvalue=0))
 		transposed_blocks = [bytes(block[i] for block in blocks) for i in range(key_size)]
-		key = bytes(decrypt_one_byte_xor(block)[1] for block in transposed_blocks)
-		keys.append((repeating_key_xor(s, key), key))
+		key = b"".join(decrypt_one_byte_xor(block).key for block in transposed_blocks)
+		keys.append(XorResult(repeating_key_xor(s, key), key))
 	return keys
 
 def decrypt_repeating_key_xor(s, min_len=2, max_len=40, keysizes_to_try=3):
-	"""returns (plaintext,key)"""
 	return max(
 		repeating_key_xor_all(s, min_len, max_len, keysizes_to_try),
 		key=lambda x: english_like(x[0].decode("utf-8", errors="ignore"))
