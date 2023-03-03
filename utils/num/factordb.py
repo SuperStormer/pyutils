e3195e516fa0c39bc7ed16b8c4ac21b37f8def8e
Larry Xue <larry.p.xue@gmail.com>
2023-03-02 22:18:37 -0500

    fix factordb

--- utils/num/factordb.py
+++ utils/num/factordb.py
@@ -5,7 +5,8 @@ from typing import List
 
 import requests
 
-URL = "http://www.factordb.com/api"
+INDEX_URL = "http://www.factordb.com/index.php"
+API_URL = "http://www.factordb.com/api"
 
 @dataclass
 class Result:
@@ -13,7 +14,9 @@ class Result:
 	factors: List[int]
 
 def fetch_result(n: int) -> Result:
-	resp = requests.get(URL, params={"query": str(n)}).json()
+	# The first request is necessary if the number isn't in the DB yet, no idea why
+	requests.get(INDEX_URL, params={"query": str(n)})
+	resp = requests.get(API_URL, params={"query": str(n)}).json()
 	return Result(
 		resp["status"], [int(factor[0]) for factor in resp["factors"] for _ in range(factor[1])]
 	)
