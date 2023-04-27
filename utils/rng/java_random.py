ab71999d329be0cf9bda6a2964981c4eff4273f5
Larry Xue <larry.p.xue@gmail.com>
2023-04-27 15:23:59 -0400

    update java rng

--- utils/rng/java_random.py
+++ utils/rng/java_random.py
@@ -16,12 +16,17 @@ class JavaRandom():
 	
 	rand = next
 	
-	def next_int(self, bound):
+	# all of the following were translated from https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
+	def next_int(self, bound=None):
+		if bound is None:
+			return self.next(32)
 		if bound <= 0:
 			raise ValueError("bound must be positive")
 		
-		if (bound & -bound) == bound:  # i.e., bound is a power of 2
+		# bound is a power of 2
+		if (bound & -bound) == bound:  # pylint: disable=invalid-unary-operand-type
 			return (bound * self.next(31)) >> 31
+		
 		bits = 0
 		val = 0
 		while bits - val + (bound - 1) < 0:
@@ -37,10 +42,17 @@ class JavaRandom():
 	
 	def next_double(self):
 		return ((self.next(26) << 27) + self.next(27)) / (1 << 53)
+	
+	nextDouble = next_double
+	nextLong = next_long
+	nextFloat = next_float
 
 # https://github.com/fta2012/ReplicatedRandom/blob/master/ReplicatedRandom.java#L32
-def find_seed(x, y):
-	""" find seed based on two next(32) calls """
+def find_seed(x, y, n=32, m=32):
+	"""
+	find seed based on two next() calls where x = next(n) and y = next(m) 
+	defaults to n = m = 32 (for two nextInts)
+	"""
 	#deal with Java signed ints
 	if x < 0:
 		x += 1 << 32
@@ -48,11 +60,11 @@ def find_seed(x, y):
 		y += 1 << 32
 	
 	mask = (1 << 48) - 1
-	upper_m_of_48_mask = ((1 << 32) - 1) << (48 - 32)
-	old_seed_upper_n = (x << (48 - 32)) & mask
-	new_seed_upper_m = (y << (48 - 32)) & mask
+	upper_m_of_48_mask = ((1 << m) - 1) << (48 - m)
+	old_seed_upper_n = (x << (48 - n)) & mask
+	new_seed_upper_m = (y << (48 - m)) & mask
 	
-	for old_seed in range(old_seed_upper_n, (old_seed_upper_n | ((1 << (48 - 32)) - 1)) + 1):
+	for old_seed in range(old_seed_upper_n, (old_seed_upper_n | ((1 << (48 - n)) - 1)) + 1):
 		new_seed = (old_seed * MULTIPLIER + ADDEND) & mask
 		if (new_seed & upper_m_of_48_mask) == new_seed_upper_m:
 			return new_seed
@@ -63,6 +75,19 @@ def find_seed_long(l):
 	y = l & 0xFFFFFFFF
 	x = (l - y) >> 32
 	return find_seed(x, y)
+
+#https://github.com/fta2012/ReplicatedRandom/blob/master/ReplicatedRandom.java#L28
+def find_seed_float(x, y):
+	""" find seed based on one nextFloat() call """
+	return find_seed(int(x * (1 << 24)), int(y * (1 << 24)), 24, 24)
+
+# https://github.com/fta2012/ReplicatedRandom/blob/master/ReplicatedRandom.java#L6
+def find_seed_double(d):
+	""" find seed based on one nextDouble() call (or Math.random())"""
+	numerator = int(d * (1 << 53))
+	first26 = numerator >> 27
+	last27 = numerator & ((1 << 27) - 1)
+	return find_seed(first26, last27, 26, 27)
 
 # https://stackoverflow.com/q/32324404
 def prev_seed(seed):
